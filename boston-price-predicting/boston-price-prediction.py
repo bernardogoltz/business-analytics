9339c090c7affbad56c7c1a0c514fb0f175f4f68
Bernardo <bergoltzx2@gmail.com>
2022-06-09 15:05:06 -0300

    boston linear regression implemented

--- boston-price-predicting/boston-price-prediction.py
+++ boston-price-predicting/boston-price-prediction.py
@@ -2,7 +2,6 @@
 
 # importing dependencies 
 import pandas as pd
-import numpy as np
 
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
@@ -15,3 +14,24 @@ df_x = pd.DataFrame(data = boston.data, columns = boston.feature_names)
 df_y = pd.DataFrame(boston.target)
 
 regression = linear_model.LinearRegression()
+
+x_train, x_test, y_train, y_test = train_test_split(df_x , df_y,test_size = 0.33 , random_state = 42)
+
+regression.fit(x_train,y_train)
+
+y_prediction = regression.predict(x_test)
+
+
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test,y_prediction)
+
+
+
+
+
+
+
+
+
+
+
